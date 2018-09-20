04d9cd9470a3866e19614d0dcfaa9fed9417f3b4
olexxxey <alexeykorneyevets@gmail.com>
2018-09-20 21:13:32 +0300

    equation reg exp

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  const a = parseInt(equation.match(/^(.*)(?= \* x\^2)/g)[0]);
+  const b = parseInt(equation.match(/(?<=x\^2 )(.*)(?= \* x)/g)[0].replace(/ /g, ''));
+  const c = parseInt(equation.match(/(?<= x )(.*)$/g)[0].replace(/ /g, ''));
+  const result = Math.round((-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a));
+  const result2 = Math.round((-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a));
+
+  return [result, result2].sort((a, b) => a-b);
 }
+
+
